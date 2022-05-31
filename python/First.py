674bc79894b9a06d548d7ee13eb6a525046da170
teacher Marcos <teacher.mrdv@gmail.com>
2022-05-31 10:32:17 +0800

    Create First.py

---
+++ python/First.py
@@ -0,0 +1,12 @@
+# Output
+print("Welcome to our first Python lesson")
+
+# Input
+name = input('What is your name? ')
+age = int(input("What is your age? "))
+height = float(input("What is your height "))
+
+# Output including what was input
+print( 'Hello ' + name )
+print( 'You said your age is ' + age + ' and your height is ' + height)
+print( 'You said your age is ' + str(age) + ' and your height is ' + str(height) )
