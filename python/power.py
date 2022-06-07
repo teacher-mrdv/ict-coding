6ae724c4fb3691a39c765f8a4431375fba7de0ac
teacher Marcos <teacher.mrdv@gmail.com>
2022-06-07 11:22:27 +0800

    Update power.py

--- python/power.py
+++ python/power.py
@@ -1,9 +1,20 @@
-def power(base, exponent):
+def positive_power(base, exponent):
     result = 1
     counter = exponent
+    print(str(base) + "^" + str(exponent) )
     while counter > 0:
+        #print( "result=" + str(result) + 'counter=' + str(counter) )
         result = result * base
         counter = counter - 1
+    #print( "result=" + str(result) + 'counter=' + str(counter) )
     return result
 
+def power(base, exponent):
+    if exponent < 0:
+        return 1/positive_power(base, -exponent)
+    else:
+        return positive_power(base, exponent)
+
 print( power(2, 8) )
+print( power(2, 0) )
+print( power(2, -2) )
